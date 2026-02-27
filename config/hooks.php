b1790a053645270734053e78bfbe2fbabfa06a53
managerpcardenas <managerpcardenas@gmail.com>
2026-02-27 00:14:10 -0500

    feat: initial release - Laravel 12 integration of voku/php-hooks

---
+++ config/hooks.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Hook Priority
+    |--------------------------------------------------------------------------
+    | Lower numbers execute first. Neutral default is 50.
+    |
+    */
+    'default_priority' => 50,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Subscribers
+    |--------------------------------------------------------------------------
+    | List your Subscriber classes here.
+    |
+    | Example:
+    |   App\Hooks\Subscribers\OrderSubscriber::class,
+    */
+    'subscribers' => [],
+];
